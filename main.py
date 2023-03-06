c3ff8f2010106320bedc621c2ee9564c52ceba9c
Chris Billows <chrisbillows@Chriss-MacBook-Pro.local>
2023-03-06 12:07:45 +0000

    add basic user input
    Add basic user input. Using HTML form method and Flask request methods. Users can now enter text and submit it to the server via a POST request, which is then displayed back to the user. Most basic example possible to explore functionality.

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 
-app = Flask('bob')
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return '<h1>Hello, World!</h1><p><a href="/howdy">If that isnt effusive enough, click here</a></p>'
+    return (
+        "<h1>Hello, World!</h1>"
+        "<p>"
+        '<a href="/howdy">If that isnt effusive enough, click here</a>'
+        "<br>"
+        '<a href="/basic_input">Enter your input</a>'
+        "</p>"
+    )
 
-@app.route('/howdy')
+    # Enter input does nothing, just links to "basic input" route/page
+
+
+@app.route("/howdy")
 def howdy():
-    return '<h1>Howdy!</h1>'
+    return "<h1>Howdy!</h1>"
+
+
+@app.route("/basic_input")
+def basic_input():
+    return (
+        '<form action="/output" method="post">'
+        '<input type="text" name="input">'
+        '<input type="submit" value="Submit"></form>'
+    )
+
+
+# action="/output" - where form data sent
+# method="post", how form data sent
+
+# on submit, 'input: {text}' is passed to request.form on the server as: <class 'werkzeug.datastructures.ImmutableMultiDict'>
+
+
+@app.route("/output", methods=["POST"])
+def output():
+    user_input = request.form["input"]
+    return f"You entered: {user_input}"
+
+
+# if methods not specified ['GET'] is the default
+# HTTP terminology client-server request methods (GET, POST, etc.) perspective of the client
+# hence POST because the client is "posting" information to the server
+
 
 app.run()
-
